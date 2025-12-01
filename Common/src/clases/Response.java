c634b36de31698761b81ca2012ac40765a429ea6
Carlos Flores <carlosvfloresv@gmail.com>
2025-11-30 22:20:30 -0500

    Added project "common" with "clases" classes

---
+++ Common/src/clases/Response.java
@@ -0,0 +1,30 @@
+package clases;
+
+public class Response {
+
+    private String status;   // "SUCCESS", "ERROR"
+    private String message;  // Mensaje para la UI
+    private Object data;     // Datos (lista de envíos, usuario, etc.)
+
+    public Response(String status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Response(String status, String message) {
+        this(status, message, null);
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
---
+++ Common/src/clases/User.java
@@ -0,0 +1,192 @@
+package clases;
+
+public class User {
+
+    private String cedula;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String numero;
+    private String ciudad;
+    private String role;
+
+    private String contrasena;
+
+    private String cargoEmpresa;
+    private String correoEmpresarial;
+
+    private String numeroLicencia;
+    private String tipoLicencia;
+    private String zonaCobertura;
+    private String disponibilidad;
+
+    private String codigoEmpleado;
+    private String estadoCuenta;
+
+    public User() {
+    }
+
+    public User(String cedula,
+                String nombre,
+                String apellidos,
+                String correo,
+                String numero,
+                String ciudad,
+                String role) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.numero = numero;
+        this.ciudad = ciudad;
+        this.role = role;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTelefono() {
+        return numero;
+    }
+
+    public void setTelefono(String telefono) {
+        this.numero = telefono;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getPassword() {
+        return contrasena;
+    }
+
+    public void setPassword(String password) {
+        this.contrasena = password;
+    }
+
+    public String getCargoEmpresa() {
+        return cargoEmpresa;
+    }
+
+    public void setCargoEmpresa(String cargoEmpresa) {
+        this.cargoEmpresa = cargoEmpresa;
+    }
+
+    public String getCorreoEmpresarial() {
+        return correoEmpresarial;
+    }
+
+    public void setCorreoEmpresarial(String correoEmpresarial) {
+        this.correoEmpresarial = correoEmpresarial;
+    }
+
+    public String getNumeroLicenia() {
+        return numeroLicencia;
+    }
+
+    public void setNumeroLicencia(String numeroLicencia) {
+        this.numeroLicencia = numeroLicencia;
+    }
+
+    public String getTipoLicencia() {
+        return tipoLicencia;
+    }
+
+    public void setTipoLicencia(String tipoLicencia) {
+        this.tipoLicencia = tipoLicencia;
+    }
+
+    public String getZonaCobertura() {
+        return zonaCobertura;
+    }
+
+    public void setZonaCobertura(String zonaCobertura) {
+        this.zonaCobertura = zonaCobertura;
+    }
+
+    public String getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public String getCodigoEmpleado() {
+        return codigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(String codigoEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+    public String getEstadoCuenta() {
+        return estadoCuenta;
+    }
+
+    public void setEstadoCuenta(String estadoCuenta) {
+        this.estadoCuenta = estadoCuenta;
+    }
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellidos;
+    }
+}
